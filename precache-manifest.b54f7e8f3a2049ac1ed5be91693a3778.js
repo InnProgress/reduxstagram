dfd15cd2b6698fcbf3fefe9fc66a77a8c0457d70
InnProgress <arnoldautuch4@gmail.com>
2019-11-30 23:24:27 +0200

    Updates

---
+++ precache-manifest.b54f7e8f3a2049ac1ed5be91693a3778.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ef5b5d26d9fc6886da4ae90e6302b0c",
+    "url": "/reduxstagram/index.html"
+  },
+  {
+    "revision": "b8363fe543f3ea6f99d3",
+    "url": "/reduxstagram/static/css/main.1860b041.chunk.css"
+  },
+  {
+    "revision": "6e4d14d64e8ecbd48f06",
+    "url": "/reduxstagram/static/js/2.b32de1cd.chunk.js"
+  },
+  {
+    "revision": "b8363fe543f3ea6f99d3",
+    "url": "/reduxstagram/static/js/main.d34ca414.chunk.js"
+  },
+  {
+    "revision": "0e6e175ca5939c22341f",
+    "url": "/reduxstagram/static/js/runtime-main.8d5556a3.js"
+  }
+]);
--- src/data/comments.js
+++
@@ -1,8 +0,0 @@
-export default [
-    {
-        id: 1,
-        postId: 1,
-        author: 'Admin',
-        text: 'Hello'
-    }
-];
